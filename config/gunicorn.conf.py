9beaf6d1275e396d8f98bf19ce87f54704faa978
mbronstein <mark@bronsteinlaw.com>
2022-04-10 19:34:42 -0400

    further changes to gunicorn conf

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -1,7 +1,7 @@
 # config/gunicorn.conf.py
 
-bind = unix:/run/gunicorn.sock
-wsgi_app= djcctest.wsgi:application
+bind = 'unix:/run/gunicorn.sock'
+wsgi_app = 'config.wsgi:application'
 backlog = 2048
 
 workers = 1
@@ -10,30 +10,19 @@ worker_connections = 1000
 timeout = 30
 keepalive = 2
 spew = False
-
-
 daemon = False
-raw_env = [
-    DJANGO_SETTINGS_FILE=config.settings.production,
-]
 
-pidfile = None
-umask = 0
-user = None
-group = None
-tmp_upload_dir = None
+raw_env = ["DJANGO_SETTINGS_FILE=config.settings.production",]
+
+
+# pidfile = None
+# umask = 0
+# user = None
+# group = None
+# tmp_upload_dir = None
 
 errorlog = '-'
 loglevel = 'info'
 accesslog = '-'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
-proc_name = None
-
-
-access - logfile - \
---workers
-3 \
-- -bind
-unix: / run / gunicorn.sock \
-    djcctest.wsgi: application
